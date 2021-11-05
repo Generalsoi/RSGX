02e5d2445a9695a01213c1d0ec9affc552f55a64
Generalsoi <ikhinobelesuccess@gmail.com>
2021-11-05 15:37:36 +0100

    updates

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ let bar2 = document.querySelector(".bar2");
 let bar3 = document.querySelector(".bar3");
 let menubar = document.querySelector(".menubar");
 let links = document.querySelector(".nav-links-div");
+let reveals = document.querySelectorAll(".reveal");
+let scrollPos = 0;
 
 menubar.addEventListener("click", function () {
   bar1.classList.toggle("barclose1");
@@ -10,3 +12,20 @@ menubar.addEventListener("click", function () {
   bar3.classList.toggle("barclose3");
   links.classList.toggle("newlinks");
 });
+
+window.addEventListener("scroll", reveal);
+
+const reveal = () => {
+  //   for (let i = 0; i < reveals.length; i++) {
+  //     // let windowHeight = window.innerHeight;
+  //     // let revealTop = reveals[i].getBoundingClientRect().top;
+  //     // let revealPoint = 150;
+  //     let windowY = window.scrollY;
+  //     if (windowY < scrollPos) {
+  //       reveals[i].classList.add("active");
+  //     } else {
+  //       reveals[i].classList.remove("active");
+  //     }
+  //     scrollPos = windowY;
+  //   }
+};
